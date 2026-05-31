f2c24bed36b09514311d6006116b3327fa210b03
Gunz-cop <228096577+Gunz-cop@users.noreply.github.com>
2026-05-31 17:05:36 -0600

    feat: unified FAQs format across all pages and open all external links in new tabs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import rehypeExternalLinks from 'rehype-external-links';
 
 import cloudflare from "@astrojs/cloudflare";
 
@@ -9,6 +10,17 @@ import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
   site: 'https://cuidatuperroviejo.com',
   integrations: [tailwind(), mdx(), sitemap()],
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: '_blank',
+          rel: ['noopener', 'noreferrer']
+        }
+      ]
+    ]
+  },
   output: "hybrid",
   adapter: cloudflare()
 });
